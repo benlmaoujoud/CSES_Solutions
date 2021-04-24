2538b2a24ac65585c43630b49fee728c8dac706d
Mohamed Benlmaoujoud <40149539+benlmaoujoud@users.noreply.github.com>
2021-04-24 18:02:18 +0000

    Update Increasing_Array.cpp

--- Increasing_Array.cpp
+++ Increasing_Array.cpp
@@ -6,10 +6,15 @@ using namespace std;
 void solve() {
     int n; 
     cin >>n ; 
-    int arr[n]; 
-    
-
-
+    int x;
+    int mx = 0;
+    ll ans =0; 
+    for(int i=0 ; i<n ; i++){ 
+        cin >>x ;   
+        mx = max(mx,x); 
+        ans+=(mx-x); 
+    }    
+    cout << ans;
 }
 
 int main() {
